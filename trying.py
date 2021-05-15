b8f851ee787abe051e91e849f0a7170dc4fc8b29
Dimetry537 <maksakov1988@gmail.com>
2021-05-15 20:47:24 +0500

    add new file with fixes for the loop

--- trying.py
+++ trying.py
@@ -6,11 +6,11 @@ size_name = int(len(name))
 size_adress = int(len(adress)
 y = ' '
 x = '|'
-for z in range(size_name):
-    a = size_adress
-    if a > z
-        print(a)
-else
-    print(z)
-print(x + name + y*z + x)
-print(x + adress + y*z + x)
+    for z in range(size_name):
+        a = size_adress
+        if a > z
+            print(a)
+    else
+        print(z)
+print(x + name + y + z + x)
+print(x + adress + y + z + x)
