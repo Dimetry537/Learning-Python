62e0ec2c6d68ed94dd266a291586862aa85f72cb
Dimetry537 <maksakov1988@gmail.com>
2021-05-15 20:13:29 +0500

    added new file trying with loop for, but it doesn't work

--- letter_to_yourself.py
+++ letter_to_yourself.py
@@ -1,24 +1,26 @@
 print('Введите свое имя')
 name = input()
 print('Введите свой адрес')
-adress =input()
+adress = input()
 x = '-'
 y = ' '
 vertic_line = '|'
 plus = '+'
 x_multiplayer = 45
-y_multiplayer = 34
+y_multiplayer = 10
 tag_multi = 7
 start_multi = 10
-plus_string = plus + x*x_multiplayer + plus
-string_tag = vertic_line + y*y_multiplayer + '####' + y*tag_multi + vertic_line
+plus_string = plus + x + plus
+string_tag = vertic_line + y*start_multi + '####' + y*tag_multi + vertic_line
 space_line = vertic_line + y*x_multiplayer
+size_name = int(len(name))
+    while size
 print(plus_string)
 print(string_tag)
 print(string_tag)
 print(string_tag)
-print(vertic_line + y*start_multi + name + vertic_line)
-print(vertic_line + y*start_multi + adress + vertic_line)
+print(vertic_line + y*start_multi + name + name_length + vertic_line)
+print(vertic_line + y*start_multi + adress + adress_length + vertic_line)
 print(space_line + vertic_line)
 print(space_line + vertic_line)
 print(plus_string)
---
+++ trying.py
@@ -0,0 +1,14 @@
+print('Напишите свое имя')
+name = input()
+print('Напишите свой адрес')
+adress = input()
+y = ' '
+x = '|'
+for z in range(int(len(name))):
+    a = int(len(adress))
+    if a > z
+        print(a)
+else
+    print(z)
+print(x + name + y*z + x)
+print(x + adress + y*z + x)
