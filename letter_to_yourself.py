2db451ee74f902e5155d2b1d4f5445aa2bc0f8cc
Dimetry537 <maksakov1988@gmail.com>
2021-05-15 12:26:23 +0500

    change file letter_to_yourself

--- letter_to_yourself.py
+++ letter_to_yourself.py
@@ -1,10 +1,24 @@
-for x in range(5):
-    print(x)
-
-# Prints out 3,4,5
-for x in range(3, 6):
-    print(x)
-
-# Prints out 3,5,7
-for x in range(3, 8, 2):
-    print(x)
+name = 'Dmitry Maksakov'
+adress = 'Somther in our world'
+x = '-'
+y = ' '
+vertic_line = '|'
+plus = '+'
+x_multiplayer = 30
+y_multiplayer = 20
+tag_multi = 7
+start_multi = 10
+plus_string = plus + x*x_multiplayer + plus
+string_tag = vertic_line + y*y_multiplayer + '####' + y*tag_multi + vertic_line
+space_line = vertic_line + y*y_multiplayer
+print(plus_string)
+print(string_tag)
+print(string_tag)
+print(string_tag)
+print(vertic_line + y*start_multi + name + vertic_line)
+print(vertic_line + y*start_multi + adress + vertic_line)
+print(space_line + y*tag_multi + vertic_line)
+print(space_line + y*tag_multi + vertic_line)
+print(plus_string)
+print(len(name))
+print(len(adress))
