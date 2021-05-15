20f8d587e5bfc60fc08a7627e78a7c8aa468fab8
Dimetry537 <maksakov1988@gmail.com>
2021-05-15 18:58:22 +0500

    change spaces and make the input

--- letter_to_yourself.py
+++ letter_to_yourself.py
@@ -1,24 +1,24 @@
-name = 'Dmitry Maksakov'
-adress = 'Somther in our world'
+print('Введите свое имя')
+name = input()
+print('Введите свой адрес')
+adress =input()
 x = '-'
 y = ' '
 vertic_line = '|'
 plus = '+'
-x_multiplayer = 30
-y_multiplayer = 20
+x_multiplayer = 45
+y_multiplayer = 34
 tag_multi = 7
 start_multi = 10
 plus_string = plus + x*x_multiplayer + plus
 string_tag = vertic_line + y*y_multiplayer + '####' + y*tag_multi + vertic_line
-space_line = vertic_line + y*y_multiplayer
+space_line = vertic_line + y*x_multiplayer
 print(plus_string)
 print(string_tag)
 print(string_tag)
 print(string_tag)
 print(vertic_line + y*start_multi + name + vertic_line)
 print(vertic_line + y*start_multi + adress + vertic_line)
-print(space_line + y*tag_multi + vertic_line)
-print(space_line + y*tag_multi + vertic_line)
+print(space_line + vertic_line)
+print(space_line + vertic_line)
 print(plus_string)
-print(len(name))
-print(len(adress))
