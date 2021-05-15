c8a77187b2eaa453908a9925b708e096fa25687b
Dimetry537 <maksakov1988@gmail.com>
2021-05-15 20:29:12 +0500

    loop of trying doesnt work

--- trying.py
+++ trying.py
@@ -2,10 +2,12 @@ print('Напишите свое имя')
 name = input()
 print('Напишите свой адрес')
 adress = input()
+size_name = int(len(name))
+size_adress = int(len(adress)
 y = ' '
 x = '|'
-for z in range(int(len(name))):
-    a = int(len(adress))
+for z in range(size_name):
+    a = size_adress
     if a > z
         print(a)
 else
