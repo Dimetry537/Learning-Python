2eb065077b669b25556b2c66240c79fa9276695f
Dimetry537 <maksakov1988@gmail.com>
2021-06-01 00:25:49 +0400

    make function of BMI formula

--- BMI_calculator.py
+++ BMI_calculator.py
@@ -1,8 +1,30 @@
-print('Metric system selection. If You want imperial metric system press 1 in system SI press 2')
-metric_system = input()
+print('Metric system selection. If You want imperial metric system press 2. If You want in system SI press 1')
+metric_system = int(input())
 print('select your weight')
 weight = float(input())
 print('select your height')
 height = float(input())
 bmi_si = weight / (height ** 2)
 bmi_imperial = (weight*703) / (height ** 2)
+def metric_system(bmi_si, bmi_imperial):
+	if metric_system < 2:
+		return bmi_si
+	else:
+		return bmi_imperial
+print('Your BMI is:' + metric_system(bmi_si, bmi_imperial))
+if metric_system(bmi_si, bmi_imperial) <= 15.0:
+	print('very severely underweight')
+elif metric_system(bmi_si, bmi_imperial) > 15.0 or metric_system(bmi_si, bmi_imperial) < 16.0:
+	print('severely underweight')
+elif metric_system(bmi_si, bmi_imperial) > 16.1 or metric_system(bmi_si, bmi_imperial) < 18.4:
+	print('underweight')
+elif metric_system(bmi_si, bmi_imperial) > 18.5 or metric_system(bmi_si, bmi_imperial) < 24.9:
+	print('normal weight')
+elif metric_system(bmi_si, bmi_imperial) > 25.0 or metric_system(bmi_si, bmi_imperial) < 29.9:
+	print('overweight')
+elif metric_system(bmi_si, bmi_imperial) > 30.0 or metric_system(bmi_si, bmi_imperial) < 34.9:
+	print('moderately obese')
+elif metric_system(bmi_si, bmi_imperial) > 35.0 or metric_system(bmi_si, bmi_imperial) < 39.9:
+	print('severely obese')
+elif metric_system(bmi_si, bmi_imperial) > 40.0:
+	print('very severely (or "morbidly") obese')
--- bmi_calculator_function.py
+++ bmi_calculator_function.py
@@ -2,7 +2,7 @@ print('select your weight')
 weight = float(input())
 print('select your height')
 height = float(input())
-def bmi_si(bmi):
+def bmi_si(**bmi):
 	bmi = weight / (height ** 2)
-	print('Your BMI is: ' + bmi)
-bmi_si(bmi)
+	print(bmi)
+bmi_si('Your BMI is: ' + bmi)
