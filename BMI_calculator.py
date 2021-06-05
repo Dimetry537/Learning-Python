400b2f890ee0e3e1349683a4c845bf3c10f0b5f4
Dimetry537 <maksakov1988@gmail.com>
2021-06-05 15:28:07 +0400

    make correction in bmi if and elif

--- BMI_calculator.py
+++ BMI_calculator.py
@@ -12,21 +12,22 @@ def bmi_formula(metric_system, weight, height):
 		return bmi_si
 	if bmi == 2:
 		return bmi_i
-print ('Your BMI is:')
-print(bmi_formula(metric_system, weight, height))
-if bmi_formula(metric_system, weight, height) <= 15.0:
+bmi_a = bmi_formula(metric_system, weight, height)
+print('Your BMI is: ')
+print(bmi_a)
+if bmi_a < 15.0:
 	print('very severely underweight')
-elif bmi_formula(metric_system, weight, height) >= 15.0 or bmi_formula(metric_system, weight, height) <= 16.0:
+elif bmi_a >= 15.0 or bmi_a <= 16.0:
 	print('severely underweight')
-elif bmi_formula(metric_system, weight, height) >= 16.1 or bmi_formula(metric_system, weight, height) <= 18.4:
+elif bmi_a >= 16.1 or bmi_a <= 18.4:
 	print('underweight')
-elif bmi_formula(metric_system, weight, height) >= 18.5 or bmi_formula(metric_system, weight, height) <= 24.9:
+elif bmi_a >= 18.5 or bmi_a <= 24.9:
 	print('normal weight')
-elif bmi_formula(metric_system, weight, height) >= 25.0 or bmi_formula(metric_system, weight, height) <= 29.9:
+elif bmi_a >= 25.0 or bmi_a <= 29.9:
 	print('overweight')
-elif bmi_formula(metric_system, weight, height) >= 30.0 or bmi_formula(metric_system, weight, height) <= 34.9:
+elif bmi_a >= 30.0 or bmi_a <= 34.9:
 	print('moderately obese')
-elif bmi_formula(metric_system, weight, height) >= 35.0 or bmi_formula(metric_system, weight, height) <= 39.9:
+elif bmi_a >= 35.0 or bmi_a <= 39.9:
 	print('severely obese')
-elif bmi_formula(metric_system, weight, height) >= 40.0:
+elif bmi_a >= 40.0:
 	print('very severely (or "morbidly") obese')
