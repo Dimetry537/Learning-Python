24e06fd4fb2631d2372392688119471f8ad6bc7a
Dimetry537 <maksakov1988@gmail.com>
2021-06-05 22:40:18 +0400

    delete basic files in learning python

--- Modules.py
+++
@@ -1,9 +0,0 @@
-import re
-
-# Your code goes here
-find_members = []
-for member in dir(re):
-    if "find" in member:
-        find_members.append(member)
-
-print(sorted(find_members))
--- first_ex.py
+++
@@ -1,5 +0,0 @@
-s = [1, 2, 3]
-t = s.copy()
-t[2] = 50
-print(s)
-print(t)
