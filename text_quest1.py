02b17815f0a4a828578e19373d27eaa8063c9ce4
Dimetry537 <maksakov1988@gmail.com>
2021-06-21 22:24:25 +0500

    make new idea of graphs in labirinth

--- text_quest1.py
+++ text_quest1.py
@@ -1,7 +1,7 @@
-up_wall = '---'
+up_wall = '--'
 lateral_wall = '|'
 spaces = '   '
-horizont_wall = [up_wall*10]
+horizont_wall = [up_wall*14]
 for i in range(len(horizont_wall)):
     print(horizont_wall[i])
 vertical_wall = [lateral_wall + spaces*9 + lateral_wall]
