4f7c4d58cc3b638f5030495b2b317b4c64e89114
dimetry537 <maksakov1988@gmail.com>
2021-07-04 21:28:50 +0500

    make first step to passing labirinth

--- text_quest_labirinth.py
+++ text_quest_labirinth.py
@@ -1,6 +1,5 @@
-#теорема планарных графов и теория смежных графов
-labirinth = [[1,2],[2,3],[2,4],[2,5],[3,6],[6,7],[7,2],[7,8],[8,9],[9,5],[9,10]]
-x = labirinth[0][0]
+labirinth = {0:[1,3,4], 1:[0,2,4], 2:[1,6], 3:[0,4,6], 4:[0,1,3,5], 5:[4], 6:[2,3]}
+x = labirinth
 rules = 'Hello, Adventure, welcome to Minotaurs labirinth. You must find exit from this labirinth and dont get caught by the minotaur. You must write in the console: left, right, up, down to go through the labirinth. Also You can write exit if you want go out from the programm, but must remember, that if back here You will start over. if you agree with the rules, write yes, if write no you will exit'
 print(rules)
 yes = input()
@@ -10,4 +9,6 @@ if yes == 'no':
 	exit()
 route = 'where will You go'
 print(route)
-print(labirinth[0][1])
+x = labirinth[0][0]
+input(x)
+if x 
