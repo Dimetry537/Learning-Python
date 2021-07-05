db158979e628c79d906ac17410468362fe9b212e
Dimetry537 <maksakov1988@gmail.com>
2021-07-05 21:58:08 +0500

    update repository

--- letter_to_yourself1.py
+++
@@ -1,9 +0,0 @@
-print('напишите своё имя')
-name = input()
-print('напишите свой адрес')
-adress = input()
-lenght_name = int(len(name))
-lenght_adress = int(len(adress))
-x = '-'
-lenght_score = lenght_name if lenght_name > lenght_adress else lenght_adress
-print(lenght_score)
--- text_quest_labirinth.py
+++ text_quest_labirinth.py
@@ -1,6 +1,6 @@
 labirinth = {0:[1,3,4], 1:[0,2,4], 2:[1,6], 3:[0,4,6], 4:[0,1,3,5], 5:[4], 6:[2,3]}
 x = labirinth
-rules = 'Hello, Adventure, welcome to Minotaurs labirinth. You must find exit from this labirinth and dont get caught by the minotaur. You must write in the console: left, right, up, down to go through the labirinth. Also You can write exit if you want go out from the programm, but must remember, that if back here You will start over. if you agree with the rules, write yes, if write no you will exit'
+rules = 'Hello, Adventurer, welcome to Minotaurs labirinth. You must find exit from this labirinth and dont get caught by the minotaur. You must write in the console: left, right, up, down to go through the labirinth. Also You can write exit if you want go out from the programm, but must remember, that if back here You will start over. if you agree with the rules, write yes, if write no you will exit'
 print(rules)
 yes = input()
 if yes == 'yes':
