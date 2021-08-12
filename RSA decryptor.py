d202320b484fce2c9561ec55e7dd626800430733
dimetry537 <maksakov1988@gmail.com>
2021-08-12 21:24:57 +0300

    complete rsa decryptor

--- RSA decryptor.py
+++ RSA decryptor.py
@@ -2,10 +2,11 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 import binascii
 
-
+public_key = bytes(str(input("Enter public key: ")), 'utf-8')
+privat_key = bytes(str(input("Enter privat key: ")), 'utf-8')
 
 msg = bytes(str(input("Enter plain text: ")), 'utf-8')
 
-decryptor = PKCS1_OAEP.new(keyPair)
-decrypted = decryptor.decrypt(encrypted)
+decryptor = PKCS1_OAEP.new(public_key, privat_key)
+decrypted = decryptor.decrypt(msg)
 print('Decrypted:', decrypted.decode('utf-8'))
