4b6914ce568bdb86eed296b4aebf6ce5d46ddba6
dimetry537 <maksakov1988@gmail.com>
2021-09-16 10:35:13 +0300

    make an identity matrix and tasks in python book

--- identity_matrix.py
+++ identity_matrix.py
@@ -1,1 +1,10 @@
+<<<<<<< HEAD
 # написать единичную матрицу с использованием циклов for
+=======
+x = [0 for i in range(16)]
+n = 4
+y = [x[i:i+n] for i in range(0,16,n)]
+z = [[1 if m == n else 0 for m, _ in enumerate(y[n])] for n, _ in enumerate(y)]
+
+for i in z: print(i)
+>>>>>>> b24fdb0 (make an identity matrix)
