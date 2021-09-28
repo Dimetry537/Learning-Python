42028763f91836891911d82155156f9e7eb9c474
Дмитрий Максаков <maksakov1988@gmail.com>
2021-09-28 22:30:41 +0300

    make code of identity matrix

--- identity_matrix.py
+++ identity_matrix.py
@@ -1,10 +1,11 @@
 # написать единичную матрицу с использованием циклов for
 #matrix = input('Ensert data of identity matrix')
 
-''' [1 0 0 0]
-    [0 1 0 0]
-    [0 0 1 0] 
-    [0 0 0 1]'''
+''' [1 0 0 0 0]
+    [0 1 0 0 0]
+    [0 0 1 0 0] 
+    [0 0 0 1 0]
+    [0 0 0 0 1]'''
 '''test_identity_matrix = str([1, 0, 0, 0]), str([0, 1, 0, 0]), str([0, 0, 1, 0]), str([0, 0, 0, 1])
 
 for x in test_identity_matrix:
@@ -13,7 +14,7 @@ for x in test_identity_matrix:
 
 print(x)
 '''
-def identity_matrix(n):
+'''def identity_matrix(n):
     m=[[0 for x in range(n)] for y in range(n)]
     for i in range(0, n):
         m[i][i] = 1
@@ -21,3 +22,11 @@ def identity_matrix(n):
 
 m = identity_matrix(6)
 print(m)
+'''
+units = 1
+zeroes = 0
+
+for x in zeroes:
+    for y in units:
+        
+    print (x)
