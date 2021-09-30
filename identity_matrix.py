067372f450f7911fc90a34736d5a1bcca0659767
Дмитрий Максаков <maksakov1988@gmail.com>
2021-09-30 22:42:37 +0300

    make an identity_matrix

--- identity_matrix.py
+++ identity_matrix.py
@@ -14,24 +14,64 @@ for x in test_identity_matrix:
 
 print(x)
 '''
-# def identity_matrix(n, m, p, i, m, t, o, a, p, 4):
-#     m=[[0 for x in range(n)] for y in range(n)]
-#     for i in range(0, n):
-#         m[i][i] = 1
-#     return m
+def identity_matrix(n):
+    m=[[0 for x in range(n)] for y in range(n)]
+    for i in range(0, n):
+        m[i][i] = 1
+    return m
 
-# m = identity_matrix(6)
-# print(m)
+print(identity_matrix(6))
 
+# TODO make this to work on code
+# [1 1 1]    [1 1 2]     [2 2 3]
+# [1 1 1] +  [1 2 1]  =  [2 3 2]
+# [1 1 1]    [2 1 1]     [3 2 2]
+
+# [1 1 1]    [1 2 2]     [1*1 + 1*1 + 1*2 1*2 + 1*2 + 1*1 1*2 + 1*1 + 1*1]     [4 5 4]
+# [1 1 1] *  [1 2 1]  =  [1*1 + 1*1 + 1*2 1*2 + 1*2 + 1*1 1*2 + 1*1 + 1*1] =   [4 5 4]
+# [1 1 1]    [2 1 1]     [1*1 + 1*1 + 1*2 1*2 + 1*2 + 1*1 1*2 + 1*1 + 1*1]     [4 5 4]
+
+# [1 0 0]    [1 2 2]     [? ? ?]     [1 2 2]
+# [0 1 0] +  [1 2 1]  =  [? ? ?] =   [1 2 1]
+# [0 0 1]    [2 1 1]     [? ? ?]     [2 1 1]
 
 # def funct_check(test, test_2, test_3, test_4, test_5, test6):
 #     print(test, test_2, test_3, test_4, test_5, test6)
+
+# x * y = 0 => x = 0 OR y = 0
 
 # funct_check(1,2,3,4,5,6)
 
 # funct_check(1,0,0,0,5,0)
 
 # funct_check(1,5,6,8,9)
+
+
+# x * y = 0
+
+# 2 * 0 = 0
+# 2 * 0 = 5 * 0
+# 2 = 5
+
+# 2 + 2 = 4
+
+# 2 + 2  = 4 + 5 * 0
+# 2 + 2 + (2 + 2)  * 0 = 4 + 5 * 0
+# 4 + (2 + 2 )* 0 - 4 = 5 * 0
+# (2 + 2 ) * 0 = 5 * 0
+# 2 + 2 = 5
+
+
+# x*e = x
+# x * y = e => y обратный элемент к x e/x
+
+# абелева группа
+# a + (b + c) = (a + b) + c - ассоциативность
+# x + (-x) = 0 - противоположные элементы
+# 0 - нулевой элемент
+# a + b = b + a - коммутативность
+
+
 
 # units = 1
 # zeroes = 0
@@ -56,14 +96,28 @@ print(x)
 #         print(list)
 
 zeroes = 0 #' 0 '
-units = 1
+unit = 1
 n = 10
-z = 0
+zero = 0
 
-matrix = [zeroes] * n
+# matrix = [zeroes] * n
 
-for x in range(n + 2):
-    print(matrix)
+# for x in range(n):
+#     # matrix = [zeroes] * n
+#     matrix[x] = unit 
+#     for y in range(x):
+#        matrix[y] = zero
+#     print(matrix)
+
+
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            print(1, end = ' ')
+        else:
+            print(0, end = ' ')
+    print('')
+
 
 # a0 = 0
 # n = 10
