19e7c9825d72cc989ffad3fed026befdf08f266e
Дмитрий Максаков <maksakov1988@gmail.com>
2021-10-13 20:23:36 +0300

    complete file how_old_are_you

--- how_old_are_you.py
+++ how_old_are_you.py
@@ -5,19 +5,15 @@ age = int(input(x))
 if age <= int(16):
     k = "You can't drive, " + name
     print(k)
-if age > int(17) and age <= int(18):
+
+if age <= int(18):
     l = "You can't vote, " + name
     print(l)
-if age > int(19) and age <= int(25):
+
+if age <= int(25):
     m = "You can't rent a car, " + name
     print(m)
+
 if age >= int(26):
     n = "You can do anything that's legal, " + name
     print(n)
-
-'''
-age is less than 16, say "You can't drive."
-age is less than 18, say "You can't vote."
-age is less than 25, say "You can't rent a car."
-age is 25 or over, say "You can do anything that's legal."
-'''
