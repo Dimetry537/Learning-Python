97d3776e51074bc10e92a11d2733c57d28e3ae2d
Дмитрий Максаков <maksakov1988@gmail.com>
2021-12-08 22:57:27 +0300

    make new task with loops

--- restore_skills.py
+++ restore_skills.py
@@ -1,3 +1,6 @@
+import array
+import time
+from math import floor, ceil
 # вывести на экран строку "Привет"
 
 # print('привет')
@@ -71,8 +74,73 @@
 
 
 #1. наисать программу которая выведет все числа которые делятся нацело на 5
-array = [1,3, 5, 10, 21, 3, 12, 5, 15]
+# array = [1,3, 5, 10, 21, 3, 12, 5, 15]
+
+# x = 5
+
+# for x in array:
+#     if x %5 == 0:
+#         print(x)
 
 #2. реализовать бинарный поиск по массиву
 # пользователь вводит искомое число
-array = [1,2,3,4,5,6] 
+#array = [1,2.5,3,4,6,7,8,9]
+# 1036
+# 3848
+
+# (3848 + 1036)/2 = 2442
+
+# (3848 - 1036)/2 + 1036 = 2442
+
+# середина = (КЗ + НЗ)/2  
+
+n = 100000000
+array = array.array('i',(i for i in range(0,n)))
+
+
+
+print('Enter your number')
+number = float(input())
+
+start_time = time.perf_counter ()
+for i in array:
+    if i == number:
+        print('found')
+end_time = time.perf_counter ()
+print(end_time - start_time, "seconds")
+
+
+start_time = time.perf_counter ()
+start = 0
+end = len(array) - 1
+middle = floor((end + start)/2)
+ 
+while True: 
+    middle_value = array[middle]
+    if middle_value == number:# ISO 754
+        print('found')
+        break
+    if middle_value > number:
+        end = middle
+        middle = floor((end + start)/2)
+    if middle_value < number:
+        start = middle
+        middle = ceil((end + start)/2)
+    if end == start:
+        print('value is not found')
+        break
+end_time = time.perf_counter ()
+print(end_time - start_time, "seconds")
+
+#  на продолжение циклов: написать програаму вычисления растояний между веторам (списками)
+
+# [1,2,2,3,3,4,4,5]
+# [2,3,4,4,5,5,6,7]
+# sqrt(1*2 + 2*3 +...)
+# почитать структуру данных стек и написать класс ее реализации (в отдельной папке strcutures)
+# class Stack:
+#   def __init__
+#   def push
+#   def pop
+# почитать обратную польскую нотацию и алгоритм преобразования в нее и вычисления по ней
+# 
