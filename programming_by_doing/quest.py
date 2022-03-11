55c591ac153d96c46ea5abe41ff3a66e28c4c7e4
Dimetry537 <maksakov1988@gmail.com>
2022-03-11 11:48:15 +0300

    quiz if statment in process

--- programming_by_doing/quest.py
+++ programming_by_doing/quest.py
@@ -9,6 +9,12 @@ You are in a creepy house!  Would you like to go "upstairs" or into the
 
 player = input('Where do you go? ')
 
-if player == 'kitchen'
-	if player == 'upstairs'
-	
+if player == 'kitchen':
+	if player == 'upstairs':
+		print('''
+Upstairs you see a hallway.  At the end of the hallway is the master "bedroom". There is also a "bathroom" off the hallway.  Where would you like to go?
+''')
+	else:
+		print('''
+There is a long countertop with dirty dishes everywhere.  Off to one side there is, as you'd expect, a refrigerator. You may open the "refrigerator" or look in a "cabinet".
+''')
