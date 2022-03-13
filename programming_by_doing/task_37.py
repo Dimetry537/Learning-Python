844b90757ce0804f15cd0d5d6a8474569565b8cd
Dimetry537 <maksakov1988@gmail.com>
2022-03-13 21:42:29 +0300

    complete task 37 pbd but with mistakes

--- programming_by_doing/task_37.py
+++ programming_by_doing/task_37.py
@@ -3,14 +3,15 @@ first_name = input('First name: ')
 last_name = input('Last name: ')
 age = int(input('Age: '))
 
-if gender == 'F' and age <= 17:
+if gender == 'F' and age >= 17:
     marriage = input('Are you married, ' + first_name + '(y or n)? ')
     if marriage == 'y':
         marriage_end = 'Mrs'
     else:
         marriage_end = 'Ms'
         print('Then I shall call you ' + marriage_end + last_name + '.')
-elif age >= 18:
-        print('Then I shall call you ' + first_name + ' ' + last_name + '.')
+if gender == 'F' and age <= 17:
+    print('Then I shall call you ' + first_name + ' ' + last_name + '.')
     
-# if gender == 'M':
+if gender == 'M':
+	print('Then I shall call you Mr. ' + last_name + '.')
