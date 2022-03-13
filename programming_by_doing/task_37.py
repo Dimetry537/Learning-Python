5991546872efa0a7e4561c395f3008727ab7f8ec
Дмитрий Максаков <maksakov1988@gmail.com>
2022-03-13 19:53:11 +0300

    rename files in programming_by_doing and started37

---
+++ programming_by_doing/task_37.py
@@ -0,0 +1,16 @@
+gender = input('What is your gender (M or F): ')
+first_name = input('First name: ')
+last_name = input('Last name: ')
+age = int(input('Age: '))
+
+if gender == 'F' and age <= 17:
+    marriage = input('Are you married, ' + first_name + '(y or n)? ')
+    if marriage == 'y':
+        marriage_end = 'Mrs'
+    else:
+        marriage_end = 'Ms'
+        print('Then I shall call you ' + marriage_end + last_name + '.')
+elif age >= 18:
+        print('Then I shall call you ' + first_name + ' ' + last_name + '.')
+    
+# if gender == 'M':
