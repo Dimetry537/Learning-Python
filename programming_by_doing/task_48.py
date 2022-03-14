d30fe7d21bd439faa947d296f189d00142750cfb
Дмитрий Максаков <maksakov1988@gmail.com>
2022-03-14 22:26:37 +0300

    complete task_48 with counter

--- programming_by_doing/task_48.py
+++ programming_by_doing/task_48.py
@@ -3,11 +3,17 @@ WELCOME TO THE BANK OF MITCHELL.
 ''')
 
 correct_pin = 1234
+loop_through = 0
 
 while True:
     enter_pin = int(input('Enter your pin: '))
     if enter_pin == correct_pin:
         exit('Your pin is correct, access is allowed')
-    else:
+    elif enter_pin != correct_pin:
         print('Pin isnt correct? try again')
+        loop_through += 1
+    if loop_through == 3:
+        print('Its last chance to enter your pin')
+    elif loop_through == 4:
+        exit('your account blocked')
     continue
