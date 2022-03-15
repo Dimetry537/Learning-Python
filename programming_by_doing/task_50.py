4e36e6c7021ad2d524ec19a0fd6fa1223e69fc44
Dimetry537 <maksakov1988@gmail.com>
2022-03-15 22:15:48 +0300

    complete task 50

--- programming_by_doing/task_50.py
+++ programming_by_doing/task_50.py
@@ -1,14 +1,16 @@
 import random
 print('HERE COMES THE DICE!')
 
-roll_1 = random.randint(1, 6)
-roll_2 = random.randint(1, 6)
-roll_total = roll_1 + roll_2
+roll_1 = 0
+roll_2 = 0
 
 while True:
-    if roll_1 == roll_2:
-        break
+    roll_1 = random.randrange(1, 6)
+    roll_2 = random.randrange(1, 6)
+    roll_total = roll_1 + roll_2
     print('roll #1: ' + str(roll_1))
     print('roll #2: ' + str(roll_2))
     print('The total is: ' + str(roll_total))
+    if roll_1 == roll_2:
+        break
         
