0a2e56bda3c45a7ef2cfbcb0c04a0f910b2e4076
Dimetry537 <maksakov1988@gmail.com>
2022-03-15 13:03:13 +0300

    complete task 49 with mistakes in loop

--- programming_by_doing/task_49.py
+++ programming_by_doing/task_49.py
@@ -7,13 +7,13 @@ number = random.randint(0, 10)
 counter = 0
 
 while True:
-	if guess < number:
-		counter +=1
-		guess = int(input('Your number is less than expected (' + str(counter) + '): '))
-	elif guess > number:
-		counter += 1
-		guess = int(input('Your number is higher (' + str(counter) + ') : '))
-	else:
-		exit('you guessed')
-	if counter == 4:
-		exit("you've run out of attempts")
+    if guess < number:
+        counter +=1
+        guess = int(input('Your number is less than expected (' + str(counter) + '): '))
+    elif guess > number:
+        counter += 1
+        guess = int(input('Your number is higher (' + str(counter) + ') : '))
+    else:
+        exit('you guessed')
+    if counter == 4:
+        exit("you've run out of attempts")
