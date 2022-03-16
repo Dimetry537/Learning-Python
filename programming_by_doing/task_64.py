8061f8ee61c9b841e8cf95c5e2f84dee8ce247f9
Dimetry537 <maksakov1988@gmail.com>
2022-03-16 20:23:54 +0300

    complete tasks 64 and 65

--- programming_by_doing/task_64.py
+++ programming_by_doing/task_64.py
@@ -4,8 +4,5 @@ message = input("Message: ")
 
 n = int(input("How many times you want to display message: "))
 
-counter = 0
-
 for i in range(n):
-    counter += 1
-    print(str(counter) + ") " + message)
+    print(str(i+1) + ") " + message)
---
+++ programming_by_doing/task_65.py
@@ -0,0 +1,4 @@
+message = "Mr. Mitchell is cool."
+
+for i in range(10):
+    print(str(i + 1) + ") " + message)
