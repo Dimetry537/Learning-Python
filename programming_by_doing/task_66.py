858e18fe40847388bfee99381345d5081025c44c
Dimetry537 <maksakov1988@gmail.com>
2022-03-17 00:52:31 +0300

    edit and complete task 66

--- programming_by_doing/task_66.py
+++ programming_by_doing/task_66.py
@@ -1,5 +1,11 @@
 count = int(input("count to: "))
-print(*range(1, count+1))
+#print(*range(1, count+1))
 #for i in range(1, count+1): print(i)
 #numbers = [i for i in range(1, count+1)]
 #print(numbers)
+list = []
+
+for i in range(1 , count+1):
+    list.append(i)
+
+print(list)
