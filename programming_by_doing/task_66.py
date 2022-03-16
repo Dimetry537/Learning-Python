98d97ced259a304a709adf99b97d77d2eea3a078
Dimetry537 <maksakov1988@gmail.com>
2022-03-17 00:13:33 +0300

    Edit task 66 without list

--- programming_by_doing/task_66.py
+++ programming_by_doing/task_66.py
@@ -1,3 +1,6 @@
 count = int(input("count to: "))
-numbers = [i for i in range(1, count+1)]
-print(numbers)
+for i in range(1, count+1):
+    k = [i]
+    print(k)
+#numbers = [i for i in range(1, count+1)]
+#print(numbers)
