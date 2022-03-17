574aff6724b57d12da6ee654fdc61298aec8f0cc
Dimetry537 <maksakov1988@gmail.com>
2022-03-17 21:32:21 +0300

    complete task 68

--- programming_by_doing/task_68.py
+++ programming_by_doing/task_68.py
@@ -4,6 +4,10 @@ ______________
 ''')
 
 x = float(-10)
-
-for i in range(x, 10, 0.5):
-    print(float(i))
+print(x)
+#for i in range(x, 10, 0.5):
+while True:
+    x += 0.5
+    print(x)
+    if x == 10:
+        break
