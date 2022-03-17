9575c8ac0370c98b3dd5dcc196bdb81421d89aa4
Dimetry537 <maksakov1988@gmail.com>
2022-03-17 07:46:36 +0300

    complete task 67

--- programming_by_doing/task_67.py
+++ programming_by_doing/task_67.py
@@ -2,7 +2,9 @@
 x = int(input("counting from: "))
 y = int(input("counting to: "))
 z = int(input("counting by: "))
+list = []
 
 for i in range(x, y + 1, z):
-    
-    print (i)
+    list.append(i)
+
+print (list)
