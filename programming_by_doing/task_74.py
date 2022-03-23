1f46b8fe049fba2b39e9f5092b7662b252822cb4
Дмитрий Максаков <maksakov1988@gmail.com>
2022-03-23 19:21:47 +0300

    complete task 74 programming_by_doing

--- programming_by_doing/task_74.py
+++ programming_by_doing/task_74.py
@@ -1,9 +1,10 @@
 number = int(input("write a number: "))
 
+s = 0
+
 for i in range(number):
     i += 1
+    s += i
     print(i , end= ",")
     
-list = [i]
-    
-print("The sum is: " + str(sum(list)))
+print("The sum is: " + str(s))
