a9b9ad7f651ca973875b7362fe42d260e5046f25
Dimetry537 <maksakov1988@gmail.com>
2022-04-19 12:58:20 +0300

    complete loop in dice

--- dice_from_koans.py
+++ dice_from_koans.py
@@ -1,11 +1,19 @@
 import random
 
-def score(dice):
-    dice = []
-    for dice in range(5):
-        roll = random.randint(1, 6)
-        for roll in dice:
-            dice.append(roll)
-            print(dice)
+dice = []
 
-score(dice=5)
+for i in range(5):
+    i = random.randint(1, 6)
+    dice.append(i)
+
+print(dice)
+
+#def score(dice):
+#    dice = []
+#    for dice in range(5):
+#        roll = random.randint(1, 6)
+#        for roll in dice:
+#            dice.append(roll)
+#            print(dice)
+
+#score(5)
