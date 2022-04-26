31efa103e48b2f80ae1d489962be3bc4760107e2
Dimetry537 <maksakov1988@gmail.com>
2022-04-26 12:40:17 +0300

    comolete function of dice in koans

--- dice_from_koans.py
+++ dice_from_koans.py
@@ -1,37 +1,63 @@
-import random
+#import random
 
-def score(dice):
+#    dice = []
 
-    dice = []
+#    for i in range(5):
+#        i = random.randint(1, 6)
+#        dice.append(i)
+#    print(dice)
+#    return dice
 
-    for i in range(5):
-        i = random.randint(1, 6)
-        dice.append(i)
-    print(dice)
-    return dice
+#others = [2, 3, 4, 6]
 
-others = [2, 3, 4, 6]
+#counter = 0
+#triplets = 0
 
-k = 0
+#for numbers in score:
+#    triplets += 1
+#    if numbers == 5:
+#        counter += 50
+#        print("we found 5")
+#        print(counter)
+#    elif triplets == 3 and numbers == 5:
+#        counter += 500
+#        print("set of 5")
+#        print(counter)
+#    elif triplets == 3 and numbers == 1:
+#        counter += 1000
+#        print("set of 1")
+#        print(counter)
+#    elif triplets == 3 and numbers == others:
+#        k += 300
+#        print("others")
+#        print(k)
+#    elif numbers == 1:
+#        counter += 100
+#        print("found 1")
+#        print(counter)
+#    else:
+#        counter += 0
+ 
+def score(dice): 
+    score = 0 
 
-for numbers in score([]):
-    if numbers == 5:
-        k += 50
-        print("we found it")
-        print(k)
-    elif numbers == 1:
-        k += 100
-        print("found 1")
-        print(k)
-    else:
-        k += 0
+    tallies = {}.fromkeys(set(dice),0) 
+    for die in dice:
+        tallies[die] += 1
+    
+    if 1 in tallies and tallies [1] >= 3:
+        tallies[1] -= 3
+        score += 1000
+    
+    for k in tallies:
+        if tallies[k] >= 3:
+            tallies[k] -= 3
+            score += k * 100
 
-n = 0
+    for k in tallies:
+        if k == 1:
+            score += 100 * tallies[k]
+        elif k == 5:
+            score += 50 * tallies[k]
         
-for sets in score([]):
-    n += 1
-    if n == 3 and sets == 5:
-        print("set 5")
-        
-    if n == 3 and sets == 1:
-        print("set 1")
+    return score
