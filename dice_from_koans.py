12050ffe9e8cf01b85abe39332f6d977c4fe1e3d
Dimetry537 <maksakov1988@gmail.com>
2022-05-02 22:35:12 +0300

    modified rules of finding variables

--- dice_from_koans.py
+++ dice_from_koans.py
@@ -1,5 +1,5 @@
 #import random
-
+#def score(dice):
 #    dice = []
 
 #    for i in range(5):
@@ -8,56 +8,62 @@
 #    print(dice)
 #    return dice
 
-#others = [2, 3, 4, 6]
+score_not_random = [1, 1, 1, 5, 5, 5]
 
-#counter = 0
-#triplets = 0
+counter = 0
+triplet = 0
 
-#for numbers in score:
-#    triplets += 1
-#    if numbers == 5:
-#        counter += 50
-#        print("we found 5")
-#        print(counter)
-#    elif triplets == 3 and numbers == 5:
-#        counter += 500
-#        print("set of 5")
-#        print(counter)
-#    elif triplets == 3 and numbers == 1:
-#        counter += 1000
-#        print("set of 1")
-#        print(counter)
-#    elif triplets == 3 and numbers == others:
-#        k += 300
-#        print("others")
-#        print(k)
-#    elif numbers == 1:
-#        counter += 100
-#        print("found 1")
-#        print(counter)
-#    else:
-#        counter += 0
- 
-def score(dice): 
-    score = 0 
-
-    tallies = {}.fromkeys(set(dice),0) 
-    for die in dice:
-        tallies[die] += 1
+for numbers in score_not_random:
     
-    if 1 in tallies and tallies [1] >= 3:
-        tallies[1] -= 3
-        score += 1000
+    triplet += 1
     
-    for k in tallies:
-        if tallies[k] >= 3:
-            tallies[k] -= 3
-            score += k * 100
-
-    for k in tallies:
-        if k == 1:
-            score += 100 * tallies[k]
-        elif k == 5:
-            score += 50 * tallies[k]
+    if numbers == 1 and triplet == 3:
+        counter += 1000
+        print(counter)
         
-    return score
+for k in score_not_random:
+        
+    triplet += 1
+        
+    if k == 5 and triplet == 3:
+        counter += 500
+        print(counter)
+    
+#    elif numbers == 5 and triplet < 3:
+#        counter += 50
+#        print(counter)
+#    
+#    elif numbers == 1 and triplet < 3:
+#        counter += 100
+#        print(counter)
+#    
+#    elif numbers == 6 and triplet == 3:
+#        counter += numbers * 100
+#        print(counter)
+        
+    
+#def score(dice): 
+#    score = 0 
+
+#    tallies = {}.fromkeys(set(dice),0) # {1:0, 5:0}
+#    for die in dice:
+#        tallies[die] += 1#{1:3, 5:2}
+#    
+#    if 1 in tallies and tallies [1] >= 3:
+#        tallies[1] -= 3
+#        score += 1000
+#    
+#    for k in tallies:
+#        if tallies[k] >= 3:
+#            tallies[k] -= 3
+#            score += k * 100
+
+#    for k in tallies:
+#        if k == 1:
+#            score += 100 * tallies[k]
+#        elif k == 5:
+#            score += 50 * tallies[k]
+#        
+#    return score
+#    
+#print(score([]))
