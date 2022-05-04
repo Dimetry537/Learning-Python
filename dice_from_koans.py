ce32729a00958d9252581c61c1434d845d7f05cb
Dimetry537 <maksakov1988@gmail.com>
2022-05-04 18:48:30 +0300

    edit variables in dice

--- dice_from_koans.py
+++ dice_from_koans.py
@@ -8,41 +8,43 @@
 #    print(dice)
 #    return dice
 
-score_not_random = [4, 4, 4]
+score_not_random = [5, 5, 5,]
 
 counter_1 = 0
 counter_2 = 0
 counter_3 = 0
-triplet = 0
+triplet_1 = 0
+triplet_2 = 0
+triplet_3 = 0
 
 for units in score_not_random:
-    triplet += 1
-    if units == 1 and  triplet < 3:
+    triplet_1 += 1
+    if units == 1 and  triplet_1 < 3:
         counter_1 += 100
     
-    elif units == 1 and triplet == 3:
+    elif units == 1 and triplet_1 == 3:
         counter_1 += 1000
     
 for fives in score_not_random:
-    triplet += 1
-    if fives == 5 and triplet < 3:
+    triplet_2 += 1
+    if fives == 5 and triplet_2 < 3:
         counter_2 += 50
          
-    elif fives == 5 and triplet == 3:
+    elif fives == 5 and triplet_2 == 3:
         counter_2 += 500
 
 for others in score_not_random:
-    triplet += 1
-    if others == 2 and triplet == 3:
+    triplet_3 += 1
+    if others == 2 and triplet_3 == 3:
         counter_3 = 2 * 100
         
-    if others == 3 and triplet == 3:
+    if others == 3 and triplet_3 == 3:
         counter_3 = 3 * 100
         
-    if others == 4 and triplet == 3:
+    if others == 4 and triplet_3 == 3:
         counter_3 = 4 * 100
     
-    if others == 6 and triplet == 3:
+    if others == 6 and triplet_3 == 3:
         counter_3 = 6 * 100
         
 counter = counter_1 + counter_2 + counter_3
