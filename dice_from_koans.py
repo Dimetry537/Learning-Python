7aebcf64e93347f44e2e94aeb6b0fdff8d87c19a
Dimetry537 <maksakov1988@gmail.com>
2022-05-04 10:39:22 +0300

    edit search in dice from koans

--- dice_from_koans.py
+++ dice_from_koans.py
@@ -8,27 +8,42 @@
 #    print(dice)
 #    return dice
 
-score_not_random = [1, 1, 1, 5, 5, 5]
+score_not_random = [5, 5, 5, 2, 2]
 
 counter = 0
 triplet = 0
 
-for numbers in score_not_random:
-    
+for units in score_not_random:
     triplet += 1
+    if units == 1 and  triplet < 3:
+        counter += 100
     
-    if numbers == 1 and triplet == 3:
+    elif units == 1 and triplet == 3:
         counter += 1000
-        print(counter)
-        
-for k in score_not_random:
-        
-    triplet += 1
-        
-    if k == 5 and triplet == 3:
-        counter += 500
-        print(counter)
     
+for fives in score_not_random:
+    triplet += 1
+    if fives == 5 and triplet < 3:
+        counter += 50
+         
+    elif fives == 5 and triplet == 3:
+        counter += 500
+
+for others in score_not_random:
+    triplet += 1
+    if others == 2 and triplet > 3:
+        counter = 2 * 100
+        
+    if others == 3 and triplet > 3:
+        counter = 3 * 100
+        
+    if others == 4 and triplet > 3:
+        counter = 4 * 100
+    
+    if others == 6 and triplet > 3:
+        counter = 6 * 100
+print(counter)
+        
 #    elif numbers == 5 and triplet < 3:
 #        counter += 50
 #        print(counter)
