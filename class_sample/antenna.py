55edef6cef237696233ef6e87e4c63970ed21fbb
Dimetry537 <maksakov1988@gmail.com>
2022-06-23 11:25:30 +0300

    make class samples for learn class in python

---
+++ class_sample/antenna.py
@@ -0,0 +1,4 @@
+class Antenna:
+    def __init__(self):
+        self.__antenna_setup = antenna_setup
+        pass
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -1,8 +1,6 @@
 class  Snake:
     
     def __init__(self):
-        self.x = x
-        self.y = y
         self.fragments = [[20, 20], [20, 21], [20, 22], [20, 23]]
         self.head = self.fragments[0]
         self.moves = None
