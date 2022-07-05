55a6bc5c6fa1d429a9f0e748de10a1416a0764aa
Dimetry537 <maksakov1988@gmail.com>
2022-07-05 10:30:00 +0300

    complete test for matrix multiplication

--- matrices.py
+++ matrices.py
@@ -1,15 +1,27 @@
-matrix_size = int(input("Введите размер квадратной матрицы: "))
+#matrix_size = int(input("Введите размер квадратной матрицы: "))
 
 #matrix_input_1 = input("Введите элементы маирицы #1 через пробел: ")
 #matrix_input_2 = input("Введите элементы маирицы #2 через пробел: ")
 
-matrix_1 = list(map(int,input("Введите элементы маирицы #1 через пробел: ").split()))
+#matrix_1 = list(map(int,input("Введите элементы маирицы #1 через пробел: ").split()))
 
-print(matrix_1)
+#print(matrix_1)
 
-first_matrix = []
+#first_matrix = []
 
-for i in range(matrix_size):
-    first_matrix.append(matrix_1)
-    
-print(first_matrix)
+#for i in range(matrix_size):
+#    first_matrix.append(matrix_1)
+#    
+#print(first_matrix)
+
+first_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+second_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+first_matrix = [[0, 0], [0, 0]]
+
+second_matrix = [[0, 0], [0, 0]]
+
+first_matrix = [[-5, - 2], [4, 6]]
+
+second_matrix = [[7, 8], [-8, -12]]
--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -1,8 +1,6 @@
+from matrices import first_matrix, second_matrix
+
 def matrix_multiplication():
-    
-    first_matrix = [[1, 2], [3, 4]]
-    
-    second_matrix = [[4, 3], [2, 6]]
     
     lenght_matrix = len(first_matrix)
     
