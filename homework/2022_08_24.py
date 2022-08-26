550333fa221802b6ccb44344559ea064600a8ccc
Dimetry537 <maksakov1988@gmail.com>
2022-08-26 09:19:56 +0300

    make first class

--- homework/2022_08_24.py
+++ homework/2022_08_24.py
@@ -1,4 +1,18 @@
+#pylint:disable=E0101
 # цель: Написать один класс полностью самостоятельно
 # 1 Задача: написать функцию расчета площади комнаты по высоте, ширине, длине (всех поверхностей)
 # 2 Задача: добавить задачу 1 через класс (class Room, 3 поля класса)
 # 3* Задача: добавить метод сравнение  комнат между собой по площади (равно не равно - def __eq__ подсказка)
+
+class Room():
+    def __init__(self, width: int, length: int, height: int) -> None:
+        self.width = width
+        self.length = length
+        self.height = height
+        self._volume = width * length * height
+        
+    def volume(self):
+        return self._volume
+        
+Room(1, 2, 3)
+Room.volume()
