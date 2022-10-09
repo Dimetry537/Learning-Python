ec41254bdc132d321b26b869bbefa830285c2858
Дмитрий Максаков <maksakov1988@gmail.com>
2022-10-09 19:22:59 +0300

    make id and date of except of ticket

--- homework/2022_08_31_overdue.py
+++ homework/2022_08_31_overdue.py
@@ -8,27 +8,33 @@
 #  4. Взять талон в работу
 # * Написать класс медика, и при взятии талона назначать медика на талон (через какой-то индентификатор - отдельное поле в классе)
 
+from tkinter.messagebox import NO
 from typing import List
+from datetime import date
 
 class Doctor:
-        def __init__(self, doctor_name: str, date_except):
+        def __init__(self, doctor_name: str):
             self.doctor_name = doctor_name
-            self.date_except = date_except
             
         def __repr__(self) -> List:
-            return f" | {self.doctor_name} | {self.date_except} |"
+            return f"{self.doctor_name}"
 
 class Ticket:
-    def __init__(self, full_name: str, birthday, age: int, date, diagnosis: str, doctor: Doctor) -> List:
+    def __init__(self, full_name: str, birthday, age: int, diagnosis: str) -> List:
         self.full_name = full_name
         self.birthday = birthday
         self.age = age
-        self.date = date
+        self.date = date.today()
         self.diagnosis = diagnosis
+        self.doctor: Doctor = None
+        self.date_except = None
+    
+    def ticket_in_work_by(self, doctor: Doctor) -> None:
         self.doctor = doctor
+        self.date_except = date.today()
         
     def __repr__(self) -> List:
-        return f"{self.full_name} | {self.birthday} | {self.age} | {self.date} | {self.diagnosis} | {self.doctor}"
+        return f"{self.full_name} | {self.birthday} | {self.age} | {self.date} | {self.diagnosis} | {self.doctor} | {self.date_except}"
         
 ticket = Ticket()
 # Tiket safe to file - Отдельный метод сохранения в фаил - отдельный метод загрузки из фаила
