0f6a543069c0f6a29d65f4c37c582378da77b403
Дмитрий Максаков <maksakov1988@gmail.com>
2022-10-15 22:50:37 +0300

    complete editing of filename of ticket

--- homework/signal_ticket/test.py
+++ homework/signal_ticket/test.py
@@ -10,6 +10,8 @@ ambulance_employee = Ambulance_employee('Сергеев А.В.', 'фельдше
 
 ticket = Ticket('Иванов И.И.', '01.01.1977', 'Пневмония', 'Поликлиника №1')
 
+ticket_name = f"{ticket.full_name} {ticket.birthday}.txt"
+
 ticket.ambulance_in_work_by(ambulance_employee)
 
 ticket.ticket_in_work_by(doctor)
@@ -18,13 +20,13 @@ doctor.write_to_file(f'{os.path.dirname(os.path.realpath(__file__))}/doctor.txt'
 
 ambulance_employee.write_to_file(f'{os.path.dirname(os.path.realpath(__file__))}/ambulance_employee.txt')
 
-ticket.write_to_file(f'{os.path.dirname(os.path.realpath(__file__))}/ticket.txt')
+ticket.write_to_file(f'{os.path.dirname(os.path.realpath(__file__))}/{ticket_name}')
 
 filename_doctor = f'{os.path.dirname(os.path.realpath(__file__))}/doctor.txt'
 
 filename_ambulance_employee = f'{os.path.dirname(os.path.realpath(__file__))}/ambulance_employee.txt'
 
-filename = f'{os.path.dirname(os.path.realpath(__file__))}/ticket.txt'
+filename = f'{os.path.dirname(os.path.realpath(__file__))}/{ticket_name}'
 
 doctor_restored = Doctor.read_from_file(filename_doctor)
 
