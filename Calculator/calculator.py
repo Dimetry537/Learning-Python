751ea2315d1e77d7164610c64d3722a5420396fd
Dimetry537 <maksakov1988@gmail.com>
2023-04-23 23:04:57 +0300

    divided tests and make privat method for postfix

---
+++ Calculator/calculator.py
@@ -0,0 +1,8 @@
+from polish_notation import ExpressionEvaluator
+
+evaluator = ExpressionEvaluator()
+infix_expression = input("Enter your expression: ")
+postfix_expression = evaluator.postfix_expression(infix_expression)
+print("Postfix expression:", postfix_expression)
+result = evaluator.compile_calculate(postfix_expression)
+print("Result:", result)
